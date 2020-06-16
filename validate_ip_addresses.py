9a395c2de4337ccbf9370fba0224ecd0c21c3739
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-06-16 19:45:38 +0530

    adding on 16-06-2020

---
+++ validate_ip_addresses.py
@@ -0,0 +1,40 @@
+def validIPAddress(s):
+    nums = '0123456789'
+    small = 'abcdef'
+    if '.' in s:
+        parts = s.split('.')
+        if len(parts)!=4:
+            return "Neither"
+        for i in parts:
+            if len(i)==0:
+                return "Neither"
+            else:
+                if i[0] == '0':
+                    return "Neither"
+                else:
+                    for j in i:
+                        if j.isalpha():
+                            return "Neither"
+                    if not 0<=int(i)<=255:
+                        return "Neither"
+        return "IPv4"
+    else:
+        parts = s.split(':')
+        if len(parts)!=8:
+            return "Neither"
+        
+        for i in parts:
+            n = len(i)
+            print(i, n)
+            if n==0:
+                return "Neither"
+            elif n>4:
+                return "Neither"
+            else:
+                for j in i:
+                    if j.isalpha():
+                        if j.lower() not in small:
+                            return "Neither"
+        return "IPv6"
+
+print(validIPAddress(input()))
