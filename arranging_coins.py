cf7cd3d95e974adad95ac313dada3c68a90f593d
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-07-01 20:14:06 +0530

    adding on 01-07-2020

---
+++ arranging_coins.py
@@ -0,0 +1,8 @@
+from math import sqrt, floor
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        a = 1
+        b = 1
+        c = -1 * (2*n)
+        D = b**2 - (4*a*c)
+        return floor(-b + floor(sqrt(D)))//(2*a) 
