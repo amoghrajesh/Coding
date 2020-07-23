a51dc1fb02eea90394977ea71b66c2ef4d493ea1
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-07-23 18:58:20 +0530

    adding on 23-07-2020

---
+++ single_number_3.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        f = 0
+        for i in nums:
+            f ^= i
+        
+        
+        ans = []
+        for i in nums:
+            x = f ^ i
+            if x in s:
+                ans.append(i)
+        return ans
