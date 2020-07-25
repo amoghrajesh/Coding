09c11ec34881ff39bed8ca9966f54b1a2a0d5721
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-07-25 21:37:54 +0530

    adding on 25-07-2020

---
+++ minimum_number_in_sorted_array_2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int m;
+        
+        while(l < r){
+            m = l + (r - l)/2;
+            if(nums[m] == nums[r]){
+                r--;
+            }
+            else if(nums[m] > nums[r]){
+                l = m + 1;
+            }
+            else if(nums[m] < nums[r]){
+                r = m;
+            }
+        }
+        return nums[l];
+        
+    }
+};
