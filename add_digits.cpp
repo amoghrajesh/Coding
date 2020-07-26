ccea4000fde5fae4aebd3796ac0c1c2af41ec120
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-07-26 12:37:52 +0530

    adding on 26-07-2020

---
+++ add_digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num < 10){
+            return num;
+        }
+        int x = num % 9;
+        if(x != 0){
+            return x;
+        }
+        return 9;
+    }
+};
