4549796bde2b4b4f99efae331f433434f04c2a4e
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-12-22 22:47:45 +0530

    adding on 22-12-2020

---
+++ December_Leetcode_Challenge/balanced_binary_tree-22.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(!root){
+            return 0;
+        }
+        
+        return 1 + max(height(root->left), height(root->right));
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        if(!root){
+            return true;
+        }
+        
+        int lh = height(root->left);
+        int rh = height(root->right);
+        
+        int d = abs(lh - rh);
+        if(d > 1){
+            return false;
+        }
+        else{
+            return isBalanced(root->left)  && isBalanced(root->right);
+        }
+    }
+};
