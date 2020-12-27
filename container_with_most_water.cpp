2bd1434ee361c8e02ba4a275b8a97e722ef32157
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-12-27 12:18:03 +0530

    adding on 26-12-2020

---
+++ container_with_most_water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area = INT_MIN;
+        int l = 0;
+        int r = height.size() - 1;
+        int a, m;
+        
+        while(l < r){
+            m = min(height[l], height[r]);
+            a = m * (r - l);
+            area = max(area, a);
+            if(m == height[l]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return area;
+    }
+};
