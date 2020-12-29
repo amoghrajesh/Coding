b71ddc85d4e492b643e3042dbcf1c0510f92033e
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2020-12-29 19:21:20 +0530

    adding on 29-12-2020

---
+++ binary_tree_pruning.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean subtreeHasOne(TreeNode root){
+        if(root == null){
+            return false;
+        }
+        boolean leftTree = subtreeHasOne(root.left);
+        if(leftTree == false){
+            root.left = null;
+        }
+        boolean rightTree = subtreeHasOne(root.right);
+        if(rightTree == false){
+            root.right = null;
+        }
+        
+        return root.val == 1 || leftTree || rightTree;
+    }
+    public TreeNode pruneTree(TreeNode root) {
+        boolean ans = subtreeHasOne(root);
+        return ans == true ? root : null;
+    }
+}
