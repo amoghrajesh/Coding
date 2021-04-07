f5d527aba8d3ace358128f2326b02a57c1331e75
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2021-04-07 14:05:42 +0530

    adding on 07-04-2021

---
+++ April_Leetcoding_Challenge_2021/determine_if_string_halves_are_alike-7.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isVowel(char c){
+        char lower = Character.toLowerCase(c);
+        return lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u';
+    }
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+        int first = 0;
+        int second = 0;
+        
+        for(int i = 0; i < n / 2; i++){
+            if(isVowel(s.charAt(i)) == true){
+                first++;
+            }
+        }
+        
+        for(int i = n / 2; i < n; i++){
+            if(isVowel(s.charAt(i)) == true){
+                second++;
+            }
+        }
+        
+        return first == second;
+    }
+}
