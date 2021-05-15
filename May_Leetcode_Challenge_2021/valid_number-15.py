b002c19fc1366dab716acbe94061edb3b02c7626
Amogh Rajesh Desai <amoghrajesh1999@gmail.com>
2021-05-15 13:08:14 +0530

    adding on 15-05-2021

---
+++ May_Leetcode_Challenge_2021/valid_number-15.py
@@ -0,0 +1,8 @@
+import re
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        pattern = re.compile(r'^[+-]?([0-9]+|([0-9]*[.][0-9]+)|([0-9]+[.][0-9]*))([e|E][+-]?[0-9]+)?$')
+        s = s.strip()
+        if not s:
+            return False
+        return pattern.match(s) != None
