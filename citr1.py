8b4c6161c136cc9b8eb685965acd90f719ccaae8
adesai <adesai@cloudera.com>
2023-08-04 11:19:25 +0530

    Making some changes to the repo

--- citr1.py
+++ citr1.py
@@ -1,15 +1,3 @@
-# def makeLargestSpecial(S):
-#         count = i = 0
-#         res = []
-#         for j, v in enumerate(S):
-#             count = count + 1 if v=='1' else count - 1
-#             if count == 0:
-#                 res.append('1' + makeLargestSpecial(S[i + 1:j]) + '0')
-#                 i = j + 1
-#         return ''.join(sorted(res)[::-1])
-
-# print(makeLargestSpecial("11011000"))
-
 import heapq
 def maxPerformance(n, speed, reliablity, maxMachines):
     
