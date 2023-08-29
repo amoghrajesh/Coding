36890c9fd2ca8bfdca6a25e3d2afcee2ee85573f
adesai <adesai@cloudera.com>
2023-08-29 15:35:01 +0530

    Correcting things

--- pyspark-file-read.py
+++ pyspark-file-read.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
   
 spark = SparkSession.builder.appName("DataFrame").getOrCreate()
   
-df = spark.read.text("file:///app/mount/Folder-1/Folder-2/input.txt")
+df = spark.read.text("file:///app/mount/input.txt")
   
 df.selectExpr("split(value, ' ') as\
 Text_Data_In_Rows_Using_Text").show(4,False)
