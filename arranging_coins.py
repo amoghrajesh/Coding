6e0d373899c9b8d02fba844c72b241ba0f25d9e6
Amogh Desai <amoghrajesh1999@gmail.com>
2024-05-28 22:13:14 +0530

    making a small change

--- arranging_coins.py
+++ arranging_coins.py
@@ -3,6 +3,6 @@ class Solution:
     def arrangeCoins(self, n: int) -> int:
         a = 1
         b = 1
-        c = -1 * (2*n)
+        c = -1 * (2 * n)
         D = b**2 - (4*a*c)
         return floor(-b + floor(sqrt(D)))//(2*a) 
